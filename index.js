82ebc8b078318eaccfd6b1db3fd995b0d8d7f51d
OKNoah <noahgray@me.com>
2017-11-16 21:41:30 -0800

    Adds twitter support

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 import amazon from 'amazon-product-api'
-import { get, orderBy } from 'lodash'
+import { get, sortBy, truncate } from 'lodash'
 import orm from 'ormjs'
+import Twitter from 'twitter'
+import superagent from 'superagent'
 
 const defaultFields = {
   platform: 'ItemAttributes[0].HardwarePlatform[0]',
   image: 'LargeImage[0].URL[0]',
   url: 'DetailPageURL[0]',
-  title: 'ItemAttributes[0].Title[0]'
+  title: 'ItemAttributes[0].Title[0]',
+  browseNode: 'BrowseNodes[0].BrowseNode[0].BrowseNodeId[0]'
+}
+
+const deafultSearch = {
+  searchIndex: 'VideoGames',
+  minPercentageOff: 20
 }
 
 export default class ProductBot {
@@ -18,14 +26,24 @@ export default class ProductBot {
     prime: 'OfferListing[0].IsEligibleForPrime[0]'
   }
 
-  constructor (credentials = {}, fields = defaultFields) {
+  constructor (credentials = {}, fields = {}) {
     this.domain = credentials.domain || 'webservices.amazon.ca'
 
-    if (fields) {
-      this.fields = {
-        ...this.fields,
-        ...fields
-      }
+    this.fields = {
+      ...this.fields,
+      ...defaultFields,
+      ...fields
+    }
+
+    if (credentials.twitter) {
+      const { consumerKey, consumerSecret, accessTokenKey, accessTokenSecret } = credentials.twitter
+
+      this.twitter = new Twitter({
+        consumer_key: consumerKey,
+        consumer_secret: consumerSecret,
+        access_token_key: accessTokenKey,
+        access_token_secret: accessTokenSecret
+      })
     }
 
     if (credentials.domain) {
@@ -43,7 +61,7 @@ export default class ProductBot {
         ASIN: { $type: String, unique: true }
       }
 
-      for (const key in fields) {
+      for (const key in {...fields, ...defaultFields}) {
         productSchema[key] = {
           $type: String,
           optional: true
@@ -69,7 +87,7 @@ export default class ProductBot {
       class Alert extends Edge {
         static schema = {
           tweeted: { $type: Boolean, optional: true },
-          twitterUrl: { $type: String, optional: true }
+          tweetId: { $type: String, optional: true }
         }
       }
 
@@ -91,14 +109,22 @@ export default class ProductBot {
 
     try {
       for (let i = 0; i < args.results; i += 10) {
-        const items = await this.client.itemSearch({
-          responseGroup: 'ItemAttributes,Offers,Images',
-          sort: 'salesrank',
-          keywords: query,
-          domain: this.domain,
-          ...args,
-          itemPage: (i / 10) + 1
-        })
+        let items = []
+
+        try {
+          items = await this.client.itemSearch({
+            responseGroup: 'ItemAttributes,Offers,Images,BrowseNodes',
+            sort: 'salesrank',
+            keywords: query,
+            domain: this.domain,
+            ...deafultSearch,
+            ...args,
+            itemPage: (i / 10) + 1
+          })
+        } catch (error) {
+          console.log(error[0])
+          break
+        }
 
         const overLimit = args.results < results.length + items.length
         const remainder = args.results % results.length
@@ -110,7 +136,7 @@ export default class ProductBot {
         }
       }
     } catch (error) {
-      console.error(error)
+      console.error(error[0].Error)
     }
 
     return results
@@ -119,7 +145,7 @@ export default class ProductBot {
   async _format (items) {
     const formatted = items.map(item => {
       const offers = get(item, 'Offers[0].Offer')
-      const sortedOffers = orderBy(offers, offer => +get(offer, 'OfferListing[0].Price[0].Amount[0]'), 0)
+      const sortedOffers = sortBy(offers, offer => +get(offer, 'OfferListing[0].Price[0].Amount[0]'), 0).reverse()
       const price = +get(sortedOffers[0], 'OfferListing[0].Price[0].Amount[0]', 0) / 100
       const discount = +get(sortedOffers[0], 'OfferListing[0].AmountSaved[0].Amount[0]', 0) / 100
       const discountPercent = +get(sortedOffers[0], 'OfferListing[0].PercentageSaved[0]', 0)
@@ -149,17 +175,15 @@ export default class ProductBot {
       let product = undefined
       let offer = undefined
       let alert = undefined
-      const { ASIN, title, platform, image, url } = item
+      const { platform } = item
 
       try {
         product = await this.Product.add({
-          ASIN,
-          title,
-          platform,
-          image,
-          url
+          ...item,
+          platform: platform && platform.toLowerCase().replace('_', '')
         })
       } catch (e) {
+        console.log('e', e)
         try {
           product = await this.Product.findOne({
             where: { ASIN: item.ASIN }
@@ -189,28 +213,101 @@ export default class ProductBot {
             product: product._id
           })
 
-          if (!offer) {
+          if (!offer && newOffer.discount > 0) {
             alert = await this.Alert.add(newOffer, newOffer)
-          } else if (offer.price < item.price) {
+          } else if (offer && offer.price < item.price) {
             alert = await this.Alert.add(offer, newOffer)
           }
         } catch (e) {
-          console.log("Can't save offer")
+          // console.log("Can't save offer")
         }
       }
 
-      if (product) {
-        return Promise.resolve({
-          product: product._id,
-          alert: alert && alert._id,
-          offer: offer && offer._id
-        })
-      } else {
-        console.log(item)
-      }
+      return Promise.resolve({
+        product: product && product._id,
+        alert: alert && alert._id,
+        offer: offer && offer._id
+      })
     }))
 
     return saves
+  }
+
+  async _getQueue (query = {}) {
+    const db = await this.Product._getDatabase()
+    let filters = ''
+
+    for (const key in query) {
+      filters += ' filter p.' + key + ' like @' + key
+    }
+
+    const aql = `
+      for p in Product
+        ${filters}
+        for o in Offer
+            filter o.product == p._id
+            for a in Alert
+                filter a._from == o._id
+                filter a.tweeted != true
+                sort a.createdAt DESC
+                return {
+                    product: p,
+                    offer: o,
+                    alert: a
+                }
+    `
+
+    const queue = await db.query({
+      query: aql,
+      bindVars: query
+    })
+
+    return queue.all()
+  }
+
+  async _tweet (data) {
+    let image = ''
+    let tweet = truncate(data.product.title, { length: 150 })
+
+    const hashtags = [
+      'Canada'
+    ]
+
+    if (data.product.platform) {
+      hashtags.push(data.product.platform)
+    }
+
+    tweet += `\n \n ${
+      hashtags.map(tag => `#${tag.replace(' ', '').replace('_', '')}`).join(' ')
+    }`
+
+    tweet += `\n \n Save $${data.offer.discount} (${data.offer.discountPercent}%) => $${data.offer.price}!`
+
+    tweet += `\n ${data.offer.prime ? '✅' : '🚫'} Prime`
+
+    tweet += `\n \n ${data.product.url}`
+
+    if (data.product.image) {
+      const imageData = await superagent.get(data.product.image)
+
+      image = await this.twitter.post('media/upload', { media: imageData.body })
+    }
+
+    const tweeted = await this.twitter.post('statuses/update', {
+      status: tweet,
+      media_ids: image.media_id_string
+    })
+
+    if (tweeted) {
+      const alert = await this.Alert.findOne({
+        where: { _id: data.alert._id }
+      })
+      alert.tweeted = true
+      alert.tweetId = tweeted.id.toString()
+      await alert.save()
+    }
+
+    return tweeted
   }
 
   /* "Public" methods */
@@ -222,10 +319,37 @@ export default class ProductBot {
     return formatted
   }
 
-  async searchAndSave (query, args = undefined) {
+  async searchAndSave (query, args) {
     const items = await this.search(query, args)
     const saved = await this._save(items)
 
     return saved
+  }
+
+  async getQueue (query = {}) {
+    const items = await this._getQueue(query)
+
+    return items
+  }
+
+  async tweetFromQueue (query = {}) {
+    const tweets = []
+    const lim = query.limit || 1
+    if (query.limit) {
+      delete query.limit
+    }
+    const items = await this.getQueue(query)
+
+    if (!items || items.length === 0) {
+      throw new Error('Nothing queued to tweet.')
+    }
+
+    for (let i = 0; i < lim; i++) {
+      const tweet = await this._tweet(items[i])
+
+      tweets.push(tweet)
+    }
+
+    return tweets
   }
 }
--- index.test.js
+++ index.test.js
@@ -9,7 +9,11 @@ const {
   awsTag,
   domain,
   arangoUrl,
-  arangoDb
+  arangoDb,
+  twitterConsumerKey,
+  twitterConsumerSecret,
+  twitterTokenKey,
+  twitterTokenSecret
 } = process.env
 
 const bot = new Bot({
@@ -18,26 +22,51 @@ const bot = new Bot({
   awsTag,
   domain,
   arangoUrl,
-  arangoDb
+  arangoDb,
+  twitter: {
+    consumerKey: twitterConsumerKey,
+    consumerSecret: twitterConsumerSecret,
+    accessTokenSecret: twitterTokenSecret,
+    accessTokenKey: twitterTokenKey
+  }
 })
 
 test('finds some products', async (t) => {
   const results = await bot.search('playstation', {
     results: 15,
     searchIndex: 'VideoGames',
-    minPercentageOff: '20'
+    minPercentageOff: 20
   })
 
   t.is(results.length, 15)
 })
 
 test('find and save to database', async (t) => {
-  const results = await bot.searchAndSave('playstation', {
+  const results = await bot.searchAndSave('', {
     results: 15,
     searchIndex: 'VideoGames',
-    minPercentageOff: '20'
+    browseNode: 16329255011,
+    minPercentageOff: 1
   })
 
   t.is(results.length, 15)
-  t.is(!!(results[0].product), true)
+  t.truthy(results[0] && results[0].product)
+})
+
+test('get alert queue', async (t) => {
+  // optional search query
+  const queue = await bot.getQueue({
+    platform: 'nintendo switch'
+  })
+
+  t.truthy(queue)
+})
+
+test('tweet from queue', async (t) => {
+  const tweeted = await bot.tweetFromQueue({
+    limit: 1,
+    place_id: '1e5cb4d0509db554'
+  })
+
+  t.truthy(tweeted)
 })
